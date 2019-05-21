dc9499ba95e91feea588ba06ee2744eb1a065e32
Aaron Webre <aaron.webre@g5platform.com>
2019-05-21 07:37:47 -0700

    More list work

--- samples/lists_append_extend.py
+++ samples/lists_append_extend.py
@@ -4,3 +4,8 @@ attendees.extend(["him", "her"])
 optional_invitees = ["us", "they"]
 potential_attendees = attendees + optional_invitees
 print("There are", len(potential_attendees), "potential attendees currently")
+
+to_line = ", ".join(attendees)
+cc_line = ", ".join(optional_invitees)
+print("To: " + to_line)
+print("Cc: " + cc_line)
